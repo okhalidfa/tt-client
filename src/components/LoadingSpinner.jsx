16ef2d545de23f98fe065e00c306e670e2bf04c8
khalid hasan <khalidhasan9953@gmail.com>
2026-07-18 21:11:26 +0600

    added LoadingSpinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ const LoadingSpinner = () => {
     <div className="flex items-center justify-center py-24">
       <div className="h-10 w-10 rounded-full border-4 border-violet-200 border-t-violet-600 animate-spin" />
     </div>
-  );
+  ); 
 };
 
 export default LoadingSpinner;
